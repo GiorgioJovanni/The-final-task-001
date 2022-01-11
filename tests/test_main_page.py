8c5af3d28c6168fcb18c9b1bf4cb8291ee2d0d60
GiorgioJovanni <ivanporozov1999@gmail.com>
2022-01-11 16:56:54 +0300

    Первый тест на основе Page Object

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,5 +1,4 @@
 from pages.main_page import MainPage
-from selenium.webdriver.common.by import By
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -7,3 +6,4 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+
