4f7e6be0c715ce7687856e0d99805e3f6e2f1531
GiorgioJovanni <ivanporozov1999@gmail.com>
2022-01-12 13:19:26 +0300

    Добавлен новый класс LoginPageLocators

--- tests/pages/locators.py
+++ tests/pages/locators.py
@@ -4,5 +4,9 @@ from selenium.webdriver.common.by import By
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")
 
+
 class LoginPageLocators:
+    ID_LOGIN = (By.CSS_SELECTOR, "#id_login-username")
+    ID_LOGIN_PASSWORD = (By.CSS_SELECTOR, "#id_login-password")
+    BUTTON_LOGIN = (By.CSS_SELECTOR, 'button[name="login_submit"]')
 
