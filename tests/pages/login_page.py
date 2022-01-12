8b4e9914c01897ef230485e72f13ba01aa48b518
GiorgioJovanni <ivanporozov1999@gmail.com>
2022-01-12 16:50:21 +0300

    Реализовал проверку на корректный url адрес, реализовал проверку, что есть форма логина, реализовал проверку, что есть форма регистрации на странице

--- tests/pages/login_page.py
+++ tests/pages/login_page.py
@@ -17,8 +17,8 @@ class LoginPage(BasePage):
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
-        assert self.is_element_present(*LoginPageLocators.ID_LOGIN_FORM), "Login form is presented"
+        assert self.is_element_present(*LoginPageLocators.ID_LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert self.is_element_present(*LoginPageLocators.ID_REGISTER_FORM), "Register form is presented"
+        assert self.is_element_present(*LoginPageLocators.ID_REGISTER_FORM), "Register form is not presented"
