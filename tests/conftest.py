c3956031c543b7e9b36358e4b68d74a3d252ad8a
GiorgioJovanni <ivanporozov1999@gmail.com>
2022-01-12 11:32:17 +0300

    Добавленны методы-проверки в Page Object

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,24 +1,21 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default="es",
-                     help="Choose browser: language")
+    parser.addoption('--language', action='store', default="ru",
+                 help="Choose language. For ex.: ru, es, en, fr")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
+    print("\nstart browser for test..")
     language = request.config.getoption("language")
-    browser = webdriver.Chrome()
-    if language == "es":
-        print("\nstart chrome browser for test..")
-        browser.get('http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/')
-    elif language != "fr":
-        print("\nlanguage should be es or fr")
-    else:
-        print("\nstart chrome browser for test..")
-        browser.get('http://selenium1py.pythonanywhere.com/fr/catalogue/coders-at-work_207/')
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language}) # en-US
+    browser = webdriver.Chrome(options=options)
+    browser.implicitly_wait(5)  # говорим WebDriver искать каждый элемент в течение 5 секунд
     yield browser
     print("\nquit browser..")
     browser.quit()
