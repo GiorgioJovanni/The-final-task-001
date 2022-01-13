263bb498e68853efa267982b9873b69d8e0f293a
GiorgioJovanni <ivanporozov1999@gmail.com>
2022-01-13 15:43:56 +0300

    Улучшен вид кода

--- tests/pages/login_page.py
+++ tests/pages/login_page.py
@@ -13,12 +13,15 @@ class LoginPage(BasePage):
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
         correct_url = self.browser.current_url
-        assert correct_url == 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
+        assert correct_url == 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/', \
+            "Login url is incorrect"
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
-        assert self.is_element_present(*LoginPageLocators.ID_LOGIN_FORM), "Login form is not presented"
+        assert self.is_element_present(*LoginPageLocators.ID_LOGIN_FORM), \
+            "Login form is not presented"
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert self.is_element_present(*LoginPageLocators.ID_REGISTER_FORM), "Register form is not presented"
+        assert self.is_element_present(*LoginPageLocators.ID_REGISTER_FORM), \
+            "Register form is not presented"
