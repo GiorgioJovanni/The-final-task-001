24ec168ddd575be1f90f1b32a9dd5e2790f48607
GiorgioJovanni <ivanporozov1999@gmail.com>
2022-01-14 13:18:43 +0300

    test 003

--- tests/pages/product_page.py
+++ tests/pages/product_page.py
@@ -3,9 +3,10 @@ from base_page import BasePage
 
 class ProductPage(BasePage):
     def should_not_be_success_message(self):
-        is_not_element_present
+        is_not_element_present = 1
 
 
     def should_dissapear_of_success_message(self):
+        a = 1
 
 
