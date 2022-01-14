60f9eb6c180fc70c0bc1cf0320d99dfb35e30575
GiorgioJovanni <ivanporozov1999@gmail.com>
2022-01-14 16:23:31 +0300

    Финальный комит 002

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -62,25 +62,4 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page_basket.should_dissapear_of_success_message()
 
 
-def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0'
-    link1 = 'http://selenium1py.pythonanywhere.com/ru/basket/'
-    page = MainPage(browser, link)
-    page.open()
-    browser.find_element(*MainPageLocators.BUTTON_GO_YO_BASKET).click()
-    # time.sleep(99999)
-    page_basket = ProductPage(browser, link1)
-    text = browser.find_element(*BasketPage.TEXT_YOUR_BASKET_IS_EMPTY).get_attribute('textContent')
-    page_basket.guest_see_text_your_basket_is_emty(text)
 
-
-def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0'
-    link1 = 'http://selenium1py.pythonanywhere.com/ru/basket/'
-    page = MainPage(browser, link)
-    page.open()
-    browser.find_element(*MainPageLocators.BUTTON_GO_YO_BASKET).click()
-    # time.sleep(99999)
-    page_basket = ProductPage(browser, link1)
-    text = browser.find_element(*BasketPage.TEXT_YOUR_BASKET_IS_EMPTY).get_attribute('textContent')
-    page_basket.guest_see_text_your_basket_is_emty(text)
