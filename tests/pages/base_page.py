7c42cb87cacb2d7d0dffbcd5bd3edbf5645dc9b1
GiorgioJovanni <ivanporozov1999@gmail.com>
2022-01-18 13:11:59 +0300

    Переместил методы go_to_login_page и should_be_login_link в BasePage

--- tests/pages/base_page.py
+++ tests/pages/base_page.py
@@ -1,6 +1,7 @@
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from tests.pages.locators import BasePageLocators
 
 
 class BasePage:
@@ -9,8 +10,9 @@ class BasePage:
         self.url = url
         self.browser.implicitly_wait(timeout)
 
-    def open(self):
-        self.browser.get(self.url)
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK_INVALID)
+        link.click()
 
     def is_element_present(self, how, what):
         try:
@@ -34,5 +36,16 @@ class BasePage:
                 until_not(EC.presence_of_element_located((how, what)))
         except TimeoutException:
             return False
-
         return True
+
+    def open(self):
+        self.browser.get(self.url)
+
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
+                                                                     " probably unauthorised user"
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
+
