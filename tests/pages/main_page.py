55751fb7bf6daaf10e1a1e866770dbdca8c712a0
GiorgioJovanni <ivanporozov1999@gmail.com>
2022-01-18 13:12:47 +0300

    Переместил метод should_be_login_link в BasePage

--- tests/pages/main_page.py
+++ tests/pages/main_page.py
@@ -1,22 +1,19 @@
 import math
 
 from tests.pages.base_page import BasePage
-from .locators import MainPageLocators, BasketPage
+from tests.pages.locators import MainPageLocators, BasketPage, BasePageLocators
+from tests.pages.login_page import LoginPage
 from selenium.common.exceptions import NoAlertPresentException
-from .login_page import LoginPage
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
         login_link.click()
         return LoginPage(browser=self.browser, url=self.browser.current_url)
 
     def should_not_be_success_message(self):
         assert self.is_not_element_present(*BasketPage.SUCCESS_MESSAGE), "Element is presented"
-
-    def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
 
     def should_be_basket_massegrs(self):
         assert self.is_element_present(*BasketPage.SUCCESS_MESSAGE), "Element is not presented"
