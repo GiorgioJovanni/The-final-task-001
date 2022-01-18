0f2d54c8cdd006ab6f3d02922a9f47b14ec45325
GiorgioJovanni <ivanporozov1999@gmail.com>
2022-01-18 13:36:22 +0300

    Добавил тетст test_guest_can_go_to_login_page_from_product_page

--- tests/pages/locators.py
+++ tests/pages/locators.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.by import By
 class BasePageLocators:
     USER_ICON = (By.CSS_SELECTOR, '.icon-user')
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
 
 class MainPageLocators:
--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -1,13 +1,5 @@
-import pytest
-import faker
-
-from selenium import webdriver
-
+from tests.pages.locators import BasePageLocators, LoginPageLocators
 from tests.pages.product_page import ProductPage
-from tests.pages.login_page import LoginPage
-from tests.pages.locators import MainPageLocators
-from tests.pages.main_page import MainPage
-from tests.pages.locators import LoginPageLocators
 
 
 def test_guest_should_see_login_link_on_product_page(browser):
@@ -16,32 +8,10 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
-# class TestUserAddToBasketFromProductPage:
-#     # @pytest.fixture
-#     # def setup(self):
-#     #     link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
-#     #     browser = webdriver.Chrome()  # открыть страницу регистрации;
-#     #     f = faker.Faker()
-#     #     email = f.email()
-#     #     password = f.password()
-#     #     LoginPage.register_new_user(email, password)  # зарегистрировать нового пользователя;
-#     #     success_mesasge = browser.find_element(LoginPageLocators.SUCCESSFUL_MESSAGE_REGISTER)  # проверить, что пользователь залогинен.
-#     #     el1 = success_mesasge.get_attribute('textContent')
-#     #     assert "Спасибо за регистрацию!" == el1, "Registretion is not success"
-#     #     return browser
-#
-#     def test_user_cant_see_success_message(self, browser):
-#         link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0'
-#         page = MainPage(browser, link)
-#         page.open()
-#         page.should_not_be_success_message()
-#
-#     def test_user_can_add_product_to_basket(self, browser):
-#         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
-#         page = MainPage(browser, link)
-#         page.open()
-#         browser.find_element(*MainPageLocators.BUTTON_ADD_TO_BASKET).click()
-#         page.should_be_basket_massegrs()
-#
-#     # def test_user_login_success(self):
-#     #     linck = ''
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    browser.find_element(*BasePageLocators.LOGIN_LINK).click()
+    page.is_element_present(*LoginPageLocators.ID_LOGIN_FORM)
