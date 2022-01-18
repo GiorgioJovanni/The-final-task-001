b23e671ecf4f2cef61893b151e67aa7da65bd344
GiorgioJovanni <ivanporozov1999@gmail.com>
2022-01-18 13:13:57 +0300

    Переместил локоторы LOGIN_LINK,LOGIN_LINK_INVALID из  MainPageLocators в BasePageLocators

--- tests/pages/locators.py
+++ tests/pages/locators.py
@@ -1,15 +1,25 @@
 from selenium.webdriver.common.by import By
 
 
+class BasePageLocators:
+    USER_ICON = (By.CSS_SELECTOR, '.icon-user')
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
+
 class MainPageLocators:
     BUTTON_GO_YO_BASKET = (By.XPATH, '//*[@id="default"]/header/div[1]/div/div[2]/span/a')
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, '.btn-add-to-basket')
 
 
 class LoginPageLocators:
     ID_LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     ID_REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    FIELD_FOR_EMAIL = (By.CSS_SELECTOR, '[name="registration-email"]')
+    FIELD_FOR_PASSWORD = (By.CSS_SELECTOR, '[name="registration-password1"]')
+    FIELD_FOR_PASSWORD_TWO = (By.CSS_SELECTOR, '[name="registration-password2"')
+    BUTTON_REGISTER = (By.CSS_SELECTOR, '[name="registration_submit"]')
+    SUCCESSFUL_MESSAGE_REGISTER = (By.CSS_SELECTOR, '.alertinner.wicon')
 
 
 class BasketPage:
