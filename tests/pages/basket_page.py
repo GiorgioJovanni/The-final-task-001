6a3222cb33e04f03129eb83da0cc479b0e74bdaf
GiorgioJovanni <ivanporozov1999@gmail.com>
2022-01-20 12:04:00 +0300

    Добавил метод для BasketPage

--- tests/pages/basket_page.py
+++ tests/pages/basket_page.py
@@ -1,4 +1,7 @@
 from tests.pages.base_page import BasePage
 
 
-# class BasketPage(BasePage):
+class BasketPage(BasePage):
+    def user_is_in_basket(self):
+        link = self.browser.current_url
+        assert "http://selenium1py.pythonanywhere.com/en-gb/basket/" == link, "User is not in basket"
